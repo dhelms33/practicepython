7af0d291c5e35b3624fc8d2bdaac63f1f6043ff7
dhelms33 <derrickhelms10@gmail.com>
2025-05-13 21:15:18 -0600

    Updated code

--- 04_divisors.py
+++ 04_divisors.py
@@ -1,16 +1,17 @@
 class Division:
     def __init__(self, user_input):
         self.user_input = user_input
-    def divisors(self, num):
-        divisor_list =[]
-        for i in range(num-1):
-            if i % 2 == 0:
+
+    def divisors(self):
+        divisor_list = []
+        for i in range(1, self.user_input):  # Start from 1 to avoid dividing by zero
+            if self.user_input % i == 0:
                 divisor_list.append(i)
         return divisor_list
-    
+
 if __name__ == "__main__":
     try:
-        num = int(input(" Please input a number to see all possible divisors: "))
+        num = int(input("Please input a number to see all possible divisors: "))
         division_obj = Division(num)
         result = division_obj.divisors()
         print(result)
