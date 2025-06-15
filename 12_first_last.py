edf20d4eab5db34930a4d2b37797f6cd9b665a42
dhelms33 <derrickhelms10@gmail.com>
2025-06-15 14:33:07 -0600

    Added draft fib sequence

--- 12_first_last.py
+++ 12_first_last.py
@@ -1,4 +1,5 @@
 #import UniqueLists
+import ast
 class UniqueFunctions():
     def __init__(self, a):
         self.a = a
@@ -6,13 +7,17 @@ class UniqueFunctions():
     def first_last(self.a):
         first_element = self.a[0]
         last_element = self.a[-1]
-        return first_element + " " + last_element
+        return str(first_element) + " " + str(last_element)
     
 if __name__ == "__main__":
     try:
         
         user_input = int(input("Please input a list of numbers to get the first and last numbers: ")) 
-        instance_unique = UniqueFunctions
-        instance_unique.first_last(user_input)
+        user_list = ast.literal_eval(user_input)
+        if not isinstance(user_list, list):
+            raise ValueError("Input is not a list.")
+        
+        instance_unique = UniqueFunctions(user_list)
+        instance_unique.first_last()
     except ValueError:
         print("This is not a list of numbers, please try again!Q")
---
+++ 13_Fibonacci.py
@@ -0,0 +1,8 @@
+class NumericFunctions:
+    def __init__(self, num):
+        self.num = num
+    
+    def fibonacci_gen(self.num):
+        fib_first = 1 + self.num
+        fib_next += fib_first
+        return fib_next
