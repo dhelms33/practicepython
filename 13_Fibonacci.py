87f024c7e98fcd9df69c988228e19f7b1fced390
dhelms33 <derrickhelms10@gmail.com>
2025-06-20 21:35:01 -0600

    Added fibonacci code

--- 13_Fibonacci.py
+++ 13_Fibonacci.py
@@ -2,7 +2,23 @@ class NumericFunctions:
     def __init__(self, num):
         self.num = num
     
-    def fibonacci_gen(self.num):
-        fib_first = 1 + self.num
-        fib_next += fib_first
-        return fib_next
+    def fibonacci_gen(self):
+        """ generates the nth fibonacci number"""
+        if self.num <= 0:
+            return 0
+        elif self.num == 1:
+            return 1
+        fib_zero, fib_one = 0,1
+        for num in range(2, self.num+1):
+            fib_zero, fib_one = fib_zero, fib_zero + fib_one
+        return fib_one
+    
+if __name__ == "__main__":
+    try:
+            user_input = int(input("Please enter a number to see the fibonacci sequence of that number: "))
+            numeric_instance = NumericFunctions(user_input)
+            result = numeric_instance.fibonacci_gen()
+            print(f"The {user_input}th Fibonacci number is {result}")
+    except ValueError:
+            print("That is not an integer, try again")
+            
