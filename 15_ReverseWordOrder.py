47831d53b1ac9eb20a8eef6ca6b448ad91eff49c
dhelms33 <derrickhelms10@gmail.com>
2025-06-25 20:07:53 -0600

    Added more fixes to 15

--- 15_ReverseWordOrder.py
+++ 15_ReverseWordOrder.py
@@ -1,18 +1,23 @@
+
 class Reverse:
     def __init__(self, ls):
         self.ls = ls
     
-    def reverse_order(self.ls):
-        reversed = ""
-        for word in range(self.ls, -1):
-            reversed = reversed + word
-        return reversed
+    def reverse_order(self):
+        words = self.ls.strip().split()
+        reversed_words = words[::-1]
+        return " ".join(reversed_words)
+    
 if __name__ == "__main__":
     try:
         user_input = input("Please input a string of multiple words")
+        if not user_input or len(user_input.split()) < 2:
+            raise ValueError("You must enter at least two words.")
         obj_instance = Reverse(user_input)
-        result = obj_instance.reverse_order
-        print(result)
-    except ValueError:
-        print("This is not a string, try again!")
+        result = obj_instance.reverse_order()
+        print("Reversed word order: "result)
+    except ValueError as ve:
+        print(f"This is not a string, try again! {ve}")
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
             
