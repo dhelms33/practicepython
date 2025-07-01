7bc32d55d966cb30816b9d635d8a4937c1898df0
dhelms33 <derrickhelms10@gmail.com>
2025-07-01 16:23:59 -0600

    Added changes to main method

--- 16_pw_gen.py
+++ 16_pw_gen.py
@@ -4,11 +4,20 @@ class Password:
     def __init__(self, first, second, last):
         self.first = first
         self.second = second
-        self.third = third
+        self.last = last
         
     def gen_simple(self):
         dictionary = ["a", ",", "9", "*", "$", "#", "@", "@@#", ":", "{}[]"]
         password = ""
         for let in range(26):
             password += dictionary[random.randint]
+        return password 
+if __name__ == "__main__":
+    try:
+        user_input = input("Do you want a 26 character password? Enter Y or N")
+        lower_ui = user_input.lower()
+        if lower_ui == "No" or user_input == "N":
+            
+        obj_instance = Password()
+            
     
