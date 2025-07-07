12ae144f64aeaa34ab550d0ce2f3e6262d51af77
dhelms33 <derrickhelms10@gmail.com>
2025-07-07 16:21:03 -0600

    Added and looked at soup doc

--- 17_decode_web.py
+++ 17_decode_web.py
@@ -1,4 +1,4 @@
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests 
 class Decoder:
     def __init__(self, ins):
@@ -8,8 +8,16 @@ class Decoder:
         help_me = print(help(requests))
         print(help_me)
     
-    def decode_webpage(self.ins):
+    def decode_webpage_text(self.ins):
         url = 'http"//github.com'
         r = requests.get(url)
+        if self.ins == 'html':
+            return r
         r_html = r.text
         return r_html
+    def decode_soup(r):
+        soup_text = BeautifulSoup(r)
+        soup_pretty = soup_text.prettify()
+        return soup_pretty
+    
+    
