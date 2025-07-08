9e83d6f6f3bd1768c570f637c3de4106e67167ea
dhelms33 <derrickhelms10@gmail.com>
2025-07-08 16:52:08 -0600

    Added main statement

--- 17_decode_web.py
+++ 17_decode_web.py
@@ -20,4 +20,12 @@ class Decoder:
         soup_pretty = soup_text.prettify()
         return soup_pretty
     
+if __name__ == "__main__":
+    try:
+        user_input = input("Please input 1 if you want your decrypted website in html or 2 if you want your decrypted website prettied using decode soup")
+        obj_instance = Decoder(user_input)
+        result = obj_instance.decode_webpage_text
+    except ValueError:
+        print("This is not one or two, please try again!")
+    
     
