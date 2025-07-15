166f536eeaf76353888fb309764081a89221f9af
dhelms33 <derrickhelms10@gmail.com>
2025-07-15 06:53:45 -0600

    Added cows and bulls

--- cows_and_bulls.py
+++ cows_and_bulls.py
@@ -15,4 +15,14 @@ class SpecificGames:
                 count_bulls +=1
             counter+=1
         return count_cows and count_bulls
+    
+if __name__ == "__main__":
+    try:
+        user_input = input("Please start guessing a letter from a word. Your correct guesses will be displayed as cows, incorrect guesses will be bulls")
+        obj_instance = SpecificGames(user_input)
+        result = obj_instance.cows_and_bulls()
+        print(result)
+    except ValueError:
+        print("This is not an accepted character, please try again.")
+    
             
