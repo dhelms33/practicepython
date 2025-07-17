f809d933ef6cdfa719cc06e46a8736112064120a
dhelms33 <derrickhelms10@gmail.com>
2025-07-17 12:08:48 -0600

    Added print_to_screen

--- 19_webpage_2.py
+++ 19_webpage_2.py
@@ -5,5 +5,17 @@ import SpecificGames
 class DecoderTwo(SpecificGames):
     def __init__(self, ui):
         self.ui = ui
+        
     def print_to_screen():
+        try:
+            url = http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture
+            result_url = requests.get(url)
+            webpage_content = ""
+            if result_url == 200:
+                webpage_content = result_url.text
+            return webpage_content
+                
+        except ValueError:
+            
+        
         
