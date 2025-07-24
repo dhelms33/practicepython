16bb2b8bc7f1c60cec897f9f761775bfe5b19d34
dhelms33 <derrickhelms10@gmail.com>
2025-07-24 17:06:52 -0600

    Added changes to 19

--- 19_webpage_2.py
+++ 19_webpage_2.py
@@ -6,7 +6,7 @@ class DecoderTwo(SpecificGames):
     def __init__(self, ui):
         self.ui = ui
         
-    def print_to_screen():
+    def print_to_screen(self):
         try:
             url = http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture
             result_url = requests.get(url)
@@ -15,7 +15,17 @@ class DecoderTwo(SpecificGames):
                 webpage_content = result_url.text
             return webpage_content
                 
-        except ValueError:
-            
+        except ValueError as ve:
+            print(f"An unknown error occurred while processing this webpage, " + {ve})
+if __name__ == "__main__":
+    user_input = input("Do you want to decode the monica-lewinsky webpage? Y or N")
+    if user_input == ("Y"):
+        obj_instance = DecoderTwo(user_input)
+        result = obj_instance.print_to_screen()
+        print(result)
+    if user_input == ("N"):
+        print("Oh well! I don't think it's anything to worry about!")
+        
+        
         
         
