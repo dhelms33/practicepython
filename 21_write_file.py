3a0404665b8a42967ca97a7d11db1d4796e1ebf7
dhelms33 <derrickhelms10@gmail.com>
2025-08-03 21:57:11 -0600

    Added new lines to 21

--- 21_write_file.py
+++ 21_write_file.py
@@ -6,6 +6,9 @@ class WriteFile(Decoder):
     def file_write(self):
         file_used = open('File_to_be_written')
         results_decoder = Decoder.decode_webpage_text()
-        written_to_file = write("test")
+        written_to_file = write(results_decoder)
         return written_to_file
+if __name__ == "__main__":
+    user_input = input("Do you want your file to write to file?")
+    
         
